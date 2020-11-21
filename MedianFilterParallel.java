3cfeed3b9dc56cd223b6dad51c116240f9c42288
Tshilidzi Mphelo <temphelo95@gmail.com>
2020-11-21 18:05:10 +0200

    Importing packages and creating a thread class inside MedianFilterParallel with its own instance variables and a  constructor

--- MedianFilterParallel.java
+++ MedianFilterParallel.java
@@ -1,3 +1,35 @@
-public class MedianFilterParallel {
+/* Tshilidzi Mphelo
+ * 21/11/2020
+ * Image Filtering
+ */
+ 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 
+public class MedianFilterParallel {
+   static class MedianFilter extends RecursiveTask<Integer[][]> { 
+        
+      private int xLow, xHigh, yLow, yHigh, filter;
+      private int[][] rgbArray;
+      private boolean splitLength;
+      
+      private int THRESHOLD = 10000;
+            
+      //Constructor
+      public MedianFilter(int xLow,int yLow,int xHigh,int yHigh,int filter,int[][] rgbArray,boolean splitLength) {
+          this.xLow = xLow;
+          this.yLow = yLow;
+          this.xHigh = xHigh;
+          this.yHigh = yHigh;
+          this.filter = filter;
+          this.rgbArray = rgbArray;
+          this.splitLength = splitLength;
+      }
+
+   }
 }
