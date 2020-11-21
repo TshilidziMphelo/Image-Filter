454c2c00e4fea321fcc98503c294353a2114b6c1
Tshilidzi Mphelo <temphelo95@gmail.com>
2020-11-21 21:50:38 +0200

    Adding the main method on MedianFilterParallel

--- MedianFilterParallel.java
+++ MedianFilterParallel.java
@@ -107,6 +107,136 @@ public class MedianFilterParallel {
 
          return pixels;
      }
+     
+     @Override
+     protected Integer[][] compute() { //Overriding compute
+
+         //Recursively dividing the image
+         if ((xHigh - xLow) * (yHigh - yLow) <= THRESHOLD) {
+             return meanFilter();
+         } else {
+
+             if (splitLength) {
+
+                 int mid = xLow + (xHigh - xLow) / 2; //Split the image from the midpoint
+                 MeanFilter left = new MeanFilter(xLow, yLow, mid, yHigh, filter, rgbArray, !splitLength); //The left part of the image
+                 MeanFilter right = new MeanFilter(mid, yLow, xHigh, yHigh, filter, rgbArray, !splitLength); //The right part of the image
+                 left.fork(); //Running the left thread
+                 Integer[][] rightSection = right.compute();//Running the right thread on the main thread
+                 Integer[][] leftSection = left.join(); //Main thread should wait for the left thread to complete
+
+                 return merge(leftSection, rightSection, splitLength); //Meging the left and right image into one
+
+             } else {
+                 //Diving the image by its height
+                 int mid = yLow + (yHigh - yLow) / 2;
+                 MeanFilter top = new MeanFilter(xLow, yLow, xHigh, mid, filter, rgbArray, !splitLength);
+                 MeanFilter bottom = new MeanFilter(xLow, mid, xHigh, yHigh, filter, rgbArray, !splitLength);
+                 top.fork();
+                 Integer[][] rightSection = bottom.compute();
+                 Integer[][] leftSection = top.join();
+
+                 return merge(leftSection, rightSection, splitLength);
+             }
+         }
+     }
+   }
+    
+    //Combines two matrices into one
+    private static Integer[][] merge(Integer[][] sectionA, Integer[][] sectionB, boolean splitLength) {
+
+        if (splitLength) { //Finding out whether its horizontal or vertical
+
+            Integer[][] res = new Integer[sectionA.length][sectionA[0].length + sectionB[0].length];  //the combined array with its size made by adding up those of the 
+                                                                                                      //arrays they're made off
+            for (int i = 0; i < res.length; i++) {
+                int index = 0;
+
+                for (int a : sectionA[i]) {
+                    res[i][index] = a;
+                    index++;
+                }
+
+                for (int b : sectionB[i]) {
+                    res[i][index] = b;
+                    index++;
+                }
+            }
+
+            return res;
+        } else {
+            int h = sectionA.length + sectionB.length;
+            Integer[][] res = new Integer[h][sectionA.length];
+
+            int index = 0;
+
+            for (Integer[] a : sectionA) {
+                res[index] = a;
+                index++;
+            }
+
+            for (Integer[] b : sectionB) {
+                res[index] = b;
+                index++;
+            }
+
+            return res;
+
+        }
+
+   }
+   
+   public static void main(String args[]) throws IOException {
+
+        //Handling terminal input
+        String path = args[0];
+        String out = args[1];
+        int filter = Integer.parseInt(args[2]);
+        
+        //Reading in the image and getting its sizes
+        BufferedImage img = ImageIO.read(new File(path));
+        int xLength = img.getWidth();
+        int yLength = img.getHeight();
+        int[][] rgbArray = new int[yLength][xLength];
+
+        //Setting the RGBs from the image onto a matrix
+        for (int y = 0; y < yLength; y++) {
+            for (int x = 0; x < xLength; x++) {
+                rgbArray[y][x] = img.getRGB(x, y);
+            }
+        }
+
+        //Creating a thread object
+        MeanFilter meanFilter = new MeanFilter(0, 0, xLength, yLength, Integer.parseInt(args[2]), rgbArray, true);
+
+        // Create the Thread Pool
+        int noOfThreads = Runtime.getRuntime().availableProcessors();
+        ForkJoinPool forkJoinPool = new ForkJoinPool(noOfThreads);
+
+        System.out.println("Performing the mean filter ...");
+        
+        long t1 = System.currentTimeMillis();
+        Integer[][] pixels = forkJoinPool.invoke(meanFilter); //invoking the thread pool
+        long lapsed = System.currentTimeMillis() - t1; //Calculating the processing time
+
+        //Writing the filtered image
+        File output = new File(out);
+        BufferedImage outImage = new BufferedImage(xLength, yLength, BufferedImage.TYPE_INT_RGB);
+
+        // Setting the new filtered pixels matrix to the output image
+        for (int x = 0; x < xLength; x++) {
+            for (int y = 0; y < yLength; y++) {
+                int rgb = pixels[y][x];
+                outImage.setRGB(x, y, rgb);
+            }
+        }
+
+        ImageIO.write(outImage, "jpg", output);
+        System.out.println("The mean filter took " + lapsed + " milliseconds to complete.");
+
+    }
+
+ 
 
    }
 }
